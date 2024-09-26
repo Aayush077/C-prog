efa9ce4b4b35734549f8f87c9d2ecc9f4e35212e
Aayush077 <aayushchaudhary998@gmail.com>
2024-09-26 23:27:45 +0530

    practice

---
+++ 1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void merge(int arr1[],int arr2[],int *a,int b)
+{
+    for(int i = 0; i < b; i++)
+    {
+        arr1[*a] = arr2[i];
+        (*a)++;
+    }
+}
+int main()
+{
+    int a, b;
+    int arr1[a], arr2[b];
+    printf("Enter the size of the first array: ");
+    scanf("%d", &a);
+    printf("Enter the size of the second array: ");
+    scanf("%d", &b);
+    printf("Enter the elements of the first array: ");
+    for(int i = 0; i < a; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+    
+    printf("Enter the elements of the second array: ");
+    for(int i = 0; i < b; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+
+    merge(arr1, arr2, &a, b);
+
+}
